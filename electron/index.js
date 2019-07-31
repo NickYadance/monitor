a83cc8bd69501c83e590ef8c639fc4fdf96ba0b2
wuyi <1271742420@qq.com>
2019-07-31 11:03:42 +0800

    [v1.0]finish
    Signed-off-by: wuyi <1271742420@qq.com>

--- electron/index.js
+++ electron/index.js
@@ -1,100 +1,73 @@
 const {app, BrowserWindow} = require('electron');
 
 let win;
-let serverProcess;
-let mainWindow;
+let appUrl = 'http://localhost:2019/main';
 
 function createWindow() {
     let platform = process.platform;
 
     if (platform === 'win32') {
-        serverProcess = require('child_process')
+        require('child_process')
             .spawn('cmd.exe', ['/c', 'demo.bat'],
                 {
                     cwd: app.getAppPath() + '/demo/bin'
                 });
     } else {
-        serverProcess = require('child_process')
-            .exec("java -jar ./lib/monitor-0.0.2-SNAPSHOT.jar", function (err, stdout, stderr) {
+        let options = {
+            name: 'Electron',
+        };
+
+        require('sudo-prompt')
+            .exec("java -jar ./lib/monitor-0.0.2-SNAPSHOT.jar", options, function (err, stdout, stderr) {
                 if (err){
                     console.error("error while invoking server process", err)
                 }
-                console.log(stdout)
+                console.log(stdout);
+                console.log(stderr);
             });
     }
-
-    if (!serverProcess) {
-        console.error('Unable to start server from ' + app.getAppPath());
-        app.quit();
-        return;
-    }
-
-    serverProcess.stdout.on('data', function (data) {
-        console.log('Server: ' + data);
-    });
-
-    console.log("Server PID: " + serverProcess.pid);
-
-    let appUrl = 'http://localhost:2019/main';
-
-    const openWindow = function () {
-        mainWindow = new BrowserWindow({
-            title: 'Bandwidth Monitor',
-            width: 800,
-            height: 800
-        });
-
-        mainWindow.loadURL(appUrl);
-
-        mainWindow.on('closed', function () {
-            mainWindow = null;
-        });
-
-        mainWindow.on('close', function (e) {
-            if (serverProcess) {
-                e.preventDefault();
-
-                // kill Java executable
-                const kill = require('tree-kill');
-                kill(serverProcess.pid, 'SIGTERM', function () {
-                    console.log('Server process killed');
-
-                    serverProcess = null;
-
-                    mainWindow.close();
-                });
-            }
-        });
-    };
-
-    const startUp = function () {
-        const requestPromise = require('minimal-request-promise');
-
-        requestPromise.get(appUrl).then(function (response) {
-            console.log('Server started!');
-            openWindow();
-        }, function (response) {
-            // console.log('Waiting for the server start...');
-
-            setTimeout(function () {
-                startUp();
-            }, 200);
-        });
-    };
-
     startUp();
 }
+
+const startUp = function () {
+    const requestPromise = require('minimal-request-promise');
+
+    requestPromise.get(appUrl).then(function (response) {
+        console.log('Server started!');
+        openWindow();
+    }, function (response) {
+        setTimeout(function () {
+            startUp();
+        }, 200);
+    });
+};
+
+const openWindow = function () {
+    win = new BrowserWindow({
+        title: 'Bandwidth Monitor',
+        width: 800,
+        height: 800
+    });
+
+    win.loadURL(appUrl);
+
+    win.on('closed', function () {
+    });
+
+    win.on('close', function (e) {
+    });
+};
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-    app.quit()
-}
-});
+        app.quit()
+    }});
 
 app.on('activate', () => {
     if (win === null) {
-    createWindow()
-}
-});
+        createWindow()
+    } else {
+        openWindow()
+    }});
